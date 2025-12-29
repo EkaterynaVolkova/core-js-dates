22fa1acf6ed0e3bd14125a6e66d182e08177e3dc
Ekateryna Volkova <volkova5005@gmail.com>
2025-12-29 17:18:49 +0200

    feat: resolve next tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -109,8 +109,12 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const daysLeft = (end.getTime() - start.getTime()) / msPerDay;
+  return Math.ceil(daysLeft) + 1;
 }
 
 /**
@@ -130,8 +134,15 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = Date.parse(period.start);
+  const end = Date.parse(period.end);
+  const check = Date.parse(date);
+  if (check >= start && check <= end) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -145,8 +156,11 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const t = new Date(date).toLocaleString('en-US', {
+    timeZone: 'UTC',
+  });
+  return t;
 }
 
 /**
@@ -161,8 +175,19 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month);
+  date.setDate(0);
+  const days = date.getDate();
+  let weekends = 0;
+  for (let i = days; i > 0; i -= 1) {
+    date.setDate(i);
+    if (date.getDay() === 0 || date.getDay() === 6) {
+      weekends += 1;
+    }
+  }
+
+  return weekends;
 }
 
 /**
